0ed3b57a4cd1443e3f99d3f2937d2135314c4321
castrolopes <50384588+castrolopes@users.noreply.github.com>
2019-05-14 22:14:17 -0300

    Create FreteDeNavio.sol

---
+++ FreteDeNavio.sol
@@ -0,0 +1,44 @@
+pragma solidity 0.5.8;
+
+    contract FreteDeNavio {
+        
+        struct Frete {
+            string nomeDoAfretador;
+            address contaDoAfretador;
+            uint valorPago;
+            string slotEscolhido;
+            uint precoDoSlot;
+            string destino;
+        }
+        constructor() public {
+            require balanceOf;
+            string name = "NAVIOFRETADOR";
+            string symbol = "SLOTS";
+            uint decimals = 0;
+            uint256 totalSupply = 1000;
+            balanceOf[msg.sender] = totalSupply;
+           }
+        function reservaDeSlot(
+            address _contaDoFretador,
+            string memory _nomeDoFretador,
+            uint _valorPago)
+            public {
+            Frete memory novoFrete = Frete (_nomeDoFretador, _contaDoFretador, _valorPago);
+            uint precoDoSlot;
+            uint numeroDeSlotsFretados;
+            uint valorTotalDoFrete;
+            uint dataDoPagamento;
+            uint valorPago;
+            }
+        function public pagarFrete(
+            address payable, donoDoNavio.transfer,
+            uint valorTotalDoFrete
+            
+            )
+        
+        valorTotalDoFrete { precoDoSlot*numeroDeSlotsFretados;
+            
+        }
+        
+        
+    }
